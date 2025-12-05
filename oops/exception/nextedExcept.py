4e1f1d7d49df6a83884e8941d2eb57567651603d
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-12-05 20:40:56 +0530

    add

---
+++ oops/exception/nextedExcept.py
@@ -0,0 +1,16 @@
+try:
+    print("start 1")
+    print("start 2")
+    try:
+        print("start 3",10/0)
+        print("start 4")
+    except:
+        print("start 5")
+    finally:
+        print("start 6")
+    print("start 7")
+except:
+    print("start 8")
+finally:
+    print("start 9")
+print("start 10")

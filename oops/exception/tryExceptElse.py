78b7548eb8a785ccf873f24e75532c1fce5107f6
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-12-10 22:06:09 +0530

    add try except else block

---
+++ oops/exception/tryExceptElse.py
@@ -0,0 +1,13 @@
+try:
+    print("try block")
+except:
+    print("except block")
+else:
+    print("else block")
+finally:
+    print("finally block")
+
+    # we not write multiple else bock for a try block.
+    # 2 we not write multiple finally block for a try block.
+    # 3. we not write multiple except block for a try block.
+    # 4. use structure (1) try block try--except--else--finally, (2) try--except--finally, (3) try--finally

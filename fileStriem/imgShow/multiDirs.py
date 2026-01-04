4d0953415963104fae6251c9e74782290b2ae73a
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-01-04 21:29:51 +0530

    add file

---
+++ fileStriem/imgShow/multiDirs.py
@@ -0,0 +1,4 @@
+import os
+
+os.makedirs("priye/sub1/sub2/sub3/sub4")
+print("directory created successfully")
---
+++ fileStriem/rvision/pieMataploti.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+sales=[43,56,87,57,43,67,97]
+plt.pie(sales)
+plt.show()
+
+import pandas

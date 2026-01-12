0c48444fa2eb74f0e6f0ef7b2d331bd58cd0f07e
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-01-12 20:38:44 +0530

    add file

---
+++ fileStriem/imgShow/renameDir.py
@@ -0,0 +1,4 @@
+import os
+
+os.rename("samDharam/subDirmysubDir",'SamDhum')
+print("dir renamed successfully")
---
+++ fileStriem/imgShow/rmDir.py
@@ -0,0 +1,4 @@
+import os
+
+os.rmdir("priye/sub1/sub2/sub3/sub4")
+print("directory deleted successfully")
